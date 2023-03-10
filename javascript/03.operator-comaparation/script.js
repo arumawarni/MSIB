2345b18ea9609a6b6274208c17c6c471f70e8154
Arum Mawarni Dwi Putri <126259680+arumawarni@users.noreply.github.com>
2023-03-10 21:11:34 +0700

    JavaScript

---
+++ javascript/03.operator-comaparation/script.js
@@ -0,0 +1,26 @@
+var a = 10;
+var a = 20;
+var a = 21;
+var a = 10;
+
+console.log("a=", a, "b=", b, "c =", c, "d =", d);
+
+console.log("a > b", a > b);
+
+console.log("a < b", a < b);
+
+console.log("a >= b", a >= b);
+
+console.log("a <= d", a <= b);
+
+console.log("a == e", a == e);
+
+console.log("a === e", a === e);
+
+console.log("c != d", c != d);
+
+console.log("c !== d", c !== d);
+
+console.log("a != e", a != e);
+
+console.log("a !== e", a !== e);
